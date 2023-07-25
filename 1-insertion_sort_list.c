6c78b9b8ea8991b97f844c17eb8fdc67970d2bc2
fahrenh8T <raymondrayn98@gmail.com>
2023-07-25 07:22:37 +0300

    1-insertion_sort_list.c mod

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,19 +8,20 @@
  *
  * Return: void
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2) {
-    if (node1->prev)
-        node1->prev->next = node2;
-    if (node2->next)
-        node2->next->prev = node1;
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	if (node2->next)
+		node2->next->prev = node1;
 
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 
-    if (!node2->prev)
-        *list = node2;
+	if (!node2->prev)
+		*list = node2;
 }
 
 /**
@@ -30,22 +31,25 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2) {
  *
  * Return: void
  */
-void insertion_sort_list(listint_t **list) {
-    listint_t *crnt, *nxt_node;
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *crnt, *nxt_node;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    crnt = (*list)->next;
+	crnt = (*list)->next;
 
-    while (crnt) {
-        nxt_node = crnt->next;
+	while (crnt)
+	{
+		nxt_node = crnt->next;
 
-        while (crnt->prev && crnt->n < crnt->prev->n) {
-            swap_nodes(list, crnt->prev, crnt);
-            print_list(*list);
-        }
+		while (crnt->prev && crnt->n < crnt->prev->n)
+		{
+			swap_nodes(list, crnt->prev, crnt);
+			print_list(*list);
+		}
 
-        crnt = nxt_node;
-    }
+		crnt = nxt_node;
+	}
 }
