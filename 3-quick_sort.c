ed3b14eda09062fac4073ca186937e5d4d17f4c2
fahrenh8T <raymondrayn98@gmail.com>
2023-07-25 07:41:20 +0300

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+
+/**
+ * swap_integers - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap_integers(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - Lomuto partition scheme for Quick sort
+ * @array: Array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ * Return: Index of the partition
+ */
+int partition(int *array, int low, int high, size_t size) {
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++) {
+		if (array[j] <= pivot) {
+			i++;
+			if (i != j)
+				swap_integers(&array[i], &array[j]);
+		}
+	}
+	if ((i + 1) != high)
+		swap_integers(&array[i + 1], &array[high]);
+
+	print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * quick_sort_aid - Helper function for Quick sort
+ * @array: Array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ */
+void quick_sort_aid(int *array, int low, int high, size_t size) {
+	if (low < high) {
+		int pi = partition(array, low, high, size);
+
+		quick_sort_aid(array, low, pi - 1, size);
+		quick_sort_aid(array, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size) {
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_aid(array, 0, size - 1, size);
+}
